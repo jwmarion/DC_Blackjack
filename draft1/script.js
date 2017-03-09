4915c54145173a38808da37df92d58ed27bcfd90
James M <jwmarion@gmail.com>
2017-03-09 11:55:24 -0500

    Finished first blackjack draft

--- draft1/script.js
+++ draft1/script.js
@@ -1,35 +1,70 @@
 $(document).ready(function () {
-var playerHand = [];
-var dealerHand =[];
-var deck = newDeck();
+  var playerHand = [];
+  var dealerHand =[];
+  var deck = newDeck();
+  var dealt = false;
+  var playerScore = 0;
+  var dealerScore = 0;
+  var pturn = true;
 
+  draw();
+   $('#reset-button').hide();
 
+  $('.buttons').on('click','#deal-button', function(e){
+    if(dealt === false){
+    initialDeal();
+    dealt = true;
+    }
+    scoring();
+    draw();
+  });
 
-dealCards(playerHand);
-dealCards(playerHand);
-draw();
+  $('.buttons').on('click','#hit-button', function(e){
+    if (playerScore < 21) {
+      dealCards(playerHand);
+      scoring();
+      draw();
+      draw();
+    }
+    if (playerScore > 21){
+      $('#messages').text('You Dead!');
+      $('#reset-button').show();
+      pturn = false;
+    }
+  });
 
-  function getCardImageUrl(inp){
-    name = '';
-    console.log(inp.point);
-    if(inp.point >=2 && inp.point <=10){
-      name = inp.point;
+  $('.buttons').on('click','#stand-button', function(e){
+    if (pturn === true){
+      pturn = false;
+      dealerTurn();
+
     }
-    else if (inp.point === 1) {
-      name = 'ace';
+  });
+
+  // $('.buttons').on('click', '#reset-button', function(e){
+  //
+  // });
+
+  function dealerTurn(){
+    while (dealerScore < 17 ) {
+      dealCards(dealerHand);
+      scoring();
+      draw();
+
     }
-    else if (inp.point === 11) {
-      name = 'jack';
+
+    if ( playerScore < 21) {
+      while(dealerScore < playerScore) {
+        dealCards(dealerHand);
+        scoring();
+        draw();
+      }
     }
-    else if (inp.point === 12) {
-      name = 'queen';
-    }
-    else if (inp.point === 13) {
-      name = 'king';
-    }
-    return('images/'+ name + '_of_' + inp.suit +'.png');
+    scoring();
+    winner();
+    draw();
+
   }
-
 
   function calculatePoints(cards){
     var total = 0;
@@ -77,45 +112,49 @@ draw();
     }
     return d;
   }
-
-  // function draw(){
   //
-  //
-  //   playerHand.forEach(function(e){
-  //     console.log(e[0].point);
-  //     var x = '';
-  //      switch(e.suit){
-  //        case 0:
-  //          x='spades';
-  //          break;
-  //        case 1:
-  //          x='hearts';
-  //          break;
-  //        case 2:
-  //          x='clubs';
-  //          break;
-  //        case 3:
-  //          x='diamonds';
-  //          break;
-  //     }
-  //     console.log('<img src=./cards/' + e.point + '_' + x + '.png');
-  //     $('#player-hand').append('<img src=./cards/' + e.point + '_' + x + '.png');
-  //   });
+  // function scoring(){
+  //   console.log(playerHand);
+  //   playerScore = playerHand.reduce(function(a,b){return a.point + b.point;});
   // }
+
+function scoring(){
+  playerScore = calculatePoints(playerHand);
+  dealerScore = calculatePoints(dealerHand);
+}
+
+
+
   function draw(){
-    console.log(playerHand);
+
+    $('#dealer-hand').children().remove();
+    $('#player-hand').children().remove();
 
     for(var n=0; n < playerHand.length; n++){
-      console.log(playerHand[n][1]);
-        console.log('<img src=./cards/' + playerHand[n].point + '_' + playerHand[n].suit + '.png');
+
       $('#player-hand').append('<img class =\'card\'src=./cards/' + playerHand[n].point + '_' + playerHand[n].suit + '.png>');
     }
 
+    for(var n=0; n < dealerHand.length; n++){
 
-
+      if (n === 0 && pturn === true){
+        var back = Math.floor((Math.random() * 9) +1);
+        console.log('<img class =\'card\'src=./cards/back' + back+'.png>');
+        $('#dealer-hand').append('<img class =\'card\'src=./cards/back' + back+'.png>');
+      }
+      else{
+        console.log('<img class =\'card\'src=./cards/' + dealerHand[n].point + '_' + dealerHand[n].suit + '.png>');
+        $('#dealer-hand').append('<img class =\'card\'src=./cards/' + dealerHand[n].point + '_' + dealerHand[n].suit + '.png>');
+      }
     }
-
-
+    $('#player-points').text(playerScore);
+  console.log('test');
+  console.log(pturn);
+    if (pturn === false){
+      console.log('test2');
+    $('#dealer-points').text(dealerScore);
+    }
+  }
 
   function initialDeal(){
     for (var i = 0; i<2; i++){
@@ -129,9 +168,29 @@ draw();
     var r = (Math.floor(
    Math.random() * deck.length));
    var t = deck.splice(r,1);
-   //console.log(t[0]);
-   //console.log(Object.values(t)[0]);
    player.push(Object.values(t)[0]);
   }
+
+  function winner(){
+    if (playerScore <= 21 && playerScore > dealerScore || playerScore <= 21 && dealerScore > 21) {
+      //fireworks
+      $('#messages').text('You win!$$$');
+       $('#reset-button').show();
+      $('body').css('background-image','url(./media/fireworks.gif)')
+    }
+    else if(playerScore > 21 && dealerScore > 21 || playerScore == dealerScore){
+      $('#messages').text('DRAW!');
+       $('#reset-button').show();
+    }
+    else{
+      $('#messages').text('You Dead!');
+       $('#reset-button').show();
+    }
+
+  }
+
+
+
+
 
 });
